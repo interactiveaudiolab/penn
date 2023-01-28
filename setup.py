03049e8261dec39fe5fbd654d85caa7a2acc7ca7
Max Morrison <maxrmorrison@gmail.com>
2023-01-28 11:56:53 -0600

    Add tensorboard dependency

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=[
         'huggingface_hub', # 0.11.1
         'numpy',           # 1.23.4
+        'tensorboard',     # 2.11.0
         'torch',           # 1.12.1+cu113
         'tqdm',            # 4.64.1
         'torchaudio',      # 0.12.1+cu113
