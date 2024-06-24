802fd205f13b696f4bdea4adc9c2ed0f2bb91f47
Tom Hunn <tom_hunn@me.com>
2024-06-24 20:26:02 +1000

    update load fn to convert stereo to mono

--- test/conftest.py
+++ test/conftest.py
@@ -14,3 +14,8 @@ import penn
 def audio():
     """Retrieve the test audio"""
     return penn.load.audio(Path(__file__).parent / 'assets' / 'gershwin.wav')
+
+@pytest.fixture(scope='session')
+def audio_stereo():
+    """Retrieve the test audio"""
+    return penn.load.audio(Path(__file__).parent / 'assets' / '500Hz_stereo.wav')
--- test/test_core.py
+++ test/test_core.py
@@ -14,3 +14,12 @@ def test_infer(audio):
         center='half-hop')
     shape = (1, audio.shape[1] // penn.HOPSIZE)
     assert pitch.shape == periodicity.shape == shape
+
+def test_infer_stereo(audio_stereo):
+    """Test that inference on stereo audio produces the correct shape"""
+    pitch, periodicity = penn.from_audio(
+        audio_stereo,
+        penn.SAMPLE_RATE,
+        center='half-hop')
+    shape = (1, audio_stereo.shape[1] // penn.HOPSIZE)
+    assert pitch.shape == periodicity.shape == shape
